dcced280213ae5fae6ec59b7052095877d9a884d
Brian Hempel <bnhempel@mtu.edu>
2018-11-21 23:12:53 -0600

    Check for changes every 30 seconds and auto-reload if changed. May not be that much better than waiting for the techstaff reload, since GH pages has its own lag.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const SECOND = 1000;
 const MINUTE = 60000;
 const addresses = [
   // Example Video
@@ -54,7 +55,45 @@ const addresses = [
   },
 ];
 
-setTimeout(() => window.location.reload(true), 10 * MINUTE);
+const pagesToCheck = [
+  'index.html',
+  'main.css',
+  'main.js'
+];
+const checkIntervalSeconds = 30;
+
+lastVersion = "";
+
+function refreshIfNeeded() {
+  var thisVersion = new Blob();
+
+  var checkPages = function(pagesToCheck) {
+    var pageToCheck = pagesToCheck[0]; // head element
+    pagesToCheck = pagesToCheck.slice(1); // rest of list
+
+    fetch(pageToCheck).then(function(response) {
+      if (response.ok) { return response.text(); }
+    }).then(function(pageText) {
+      thisVersion += pageText;
+      if (pagesToCheck.length > 0) {
+        checkPages(pagesToCheck);
+      } else {
+        // console.log(thisVersion);
+        if (window.lastVersion.length == 0) {
+          window.lastVersion = thisVersion;
+        } else if (window.lastVersion !== thisVersion) {
+          // console.log(window.lastVersion);
+          // console.log(thisVersion);
+          window.location.reload(true);
+        }
+        window.setTimeout(refreshIfNeeded, checkIntervalSeconds * SECOND);
+      }
+    })
+  }
+
+  checkPages(pagesToCheck);
+}
+refreshIfNeeded(); // Load up the initial version to compare against.
 
 let idx = 0;
 function setPage() {
--- readmd.md
+++ readmd.md
@@ -2,3 +2,13 @@ Screen saver for uchicago vis+pl lab
 
 Dependencies are present to provide infra support, namely linting.
 To install, run yarn, then to lint, run yarn lint. Easy peasy.
+
+If you're trying to test the auto-reload locally, you'll have to run through a server using e.g. one of:
+
+```
+$ ruby -run -ehttpd . -p8000
+$ python2 -m SimpleHTTPServer
+$ python3 -m http.server 8000
+```
+
+...and head to [http://localhost:8000/](http://localhost:8000/).
